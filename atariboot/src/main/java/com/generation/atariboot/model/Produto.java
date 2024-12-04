1ef1a2942cdbd45c6d6ab0c1a7d0ab9c11d4f52a
USER <blancasuarz@gmail.com>
2024-12-04 10:08:10 -0300

    Classe Produto

---
+++ atariboot/src/main/java/com/generation/atariboot/model/Produto.java
@@ -0,0 +1,34 @@
+package com.generation.atariboot.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo título é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+
+	@NotBlank(message = "O atributo texto é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+}
--- atariboot/src/test/java/com/generation/atariboot/model/Produto.java
+++
@@ -1,5 +0,0 @@
-package com.generation.atariboot.model;
-
-public class Produto {
-
-}
